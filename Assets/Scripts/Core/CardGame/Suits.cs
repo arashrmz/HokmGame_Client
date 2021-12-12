88497ad84c9e0685fcd25a8b6fda054f38880e28
Arash <arash1.ramzani@gmail.com>
2021-12-12 16:47:26 +0330

    [Add] Suit enum

---
+++ Assets/Scripts/Core/CardGame/Suits.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HokmGame.Core.CardGame
+{
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade
+    }
+
+    public static class SuitInfo
+    {
+        private static readonly string[] _suitNames = new string[]
+        {
+            "Club",
+            "Diamond",
+            "Heart",
+            "Spade"
+        };
+
+        public static string GetSuitName(Suit suit)
+        {
+            return _suitNames[(int)suit];
+        }
+
+        public static Suit? FromString(string name)
+        {
+            return name switch
+            {
+                "Club" => Suit.Club,
+                "Diamond" => Suit.Diamond,
+                "Heart" => Suit.Heart,
+                "Spade" => Suit.Spade,
+                _ => null
+            };
+        }
+    }
+}
