c0803e189fcafa1585af0b4401821d51cbbd9224
Arash <arash1.ramzani@gmail.com>
2021-12-13 18:53:22 +0330

    [Add] GameScoreStatus enum

---
+++ Assets/Scripts/Core/Hokm/GameScoreStatus.cs
@@ -0,0 +1,11 @@
+namespace HokmGame.Core.Hokm
+{
+    public enum GameScoreStatus
+    {
+        NotStarted,
+        Playing,
+        Won = 10,
+        WhiteWash, //kot
+        TrumpCallerWhiteWash //kot hakem
+    }
+}
