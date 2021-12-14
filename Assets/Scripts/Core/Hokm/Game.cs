08fac2b648219908d931d346e07428073402d3d7
Arash <arash1.ramzani@gmail.com>
2021-12-14 18:55:07 +0330

    Finished Game class

--- Assets/Scripts/Core/Hokm/Game.cs
+++ Assets/Scripts/Core/Hokm/Game.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using HokmGame.Core.CardGame;
 using HokmGame.Core.Hokm.Events;
@@ -100,5 +101,164 @@ namespace HokmGame.Core.Hokm
         {
             TrickStarted?.Invoke(this, args);
         }
+
+        //deal the cards and select trump suit
+        public async Task<Suit?> StartAndDealAsync(CancellationToken cancellationToken)
+        {
+            var playingOrder = BuildPlayingOrder(TrumpCaller);
+            var trumpCaller = GetPlayer(TrumpCaller);
+            var deck = new Deck(_shuffler).Shuffle();
+
+            //deal 5 cards to each player
+            foreach (var position in playingOrder)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    return null;
+
+                var cards = deck.Deal(5).ToArray();
+                var player = GetPlayer(position);
+                _playerShadows[position].ReceiveHand(cards);
+                await player.ReceiveHandAsync(cards);
+                //wait for trump caller to call the trump suit
+                if (position == TrumpCaller)
+                {
+                    _trumpSuit = await trumpCaller.CallTrumpSuitAsync();
+                }
+            }
+
+            //deal the rest of the cards(4 cards each round & 2 rounds total)
+            for (int i = 0; i < 2; i++)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    return null;
+                foreach (var position in playingOrder)
+                {
+                    var cards = deck.Deal(4).ToArray();
+                    var player = GetPlayer(position);
+                    _playerShadows[position].ReceiveHand(cards);
+                    await player.ReceiveHandAsync(cards);
+                }
+            }
+
+            OnCardsDealt(new CardsDealtEventArgs()
+            {
+                TrumpSuit = _trumpSuit,
+                Hands = _playerShadows.ToDictionary(x => x.Key, kv => kv.Value._handCards.Select(y => y))
+            });
+
+            return _trumpSuit;
+        }
+
+        //play a trick async
+        public async Task<TrickOutcome> PlayTrickAsync(CancellationToken cancellationToken, TimeSpan? inBetweenDelay = null)
+        {
+            _currentTrickNumber++;
+            OnTrickStarted(EventArgs.Empty);
+            var playingOrder = BuildPlayingOrder(_currentTrickStarter);
+            var cardsPlayed = new List<Card>();
+            CurrentTrick = new TrickInfo()
+            {
+                TrickNumber = _currentTrickNumber,
+                Starter = _currentTrickStarter
+            };
+
+            foreach (var position in playingOrder)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    return null;
+                var p = GetPlayer(position);
+                var card = await p.PlayAsync(_currentTrickNumber, cardsPlayed, _trumpSuit);
+                var result = _playerShadows[position].ValidateAndPlay(card, cardsPlayed.Count == 0 ? card.Suit : cardsPlayed[0].Suit);
+
+                if (!result.IsValid)
+                {
+                    throw new InvalidPlayException($"{position}: {result.ErrorMessage}");
+                    //play instead of player
+                }
+
+                cardsPlayed.Add(card);
+                CurrentTrick.CardsPlayed = cardsPlayed.ToArray();
+                CurrentTrick.CurrentWinningPosition = playingOrder[DecideWinnerCard(cardsPlayed, _trumpSuit)];
+
+                OnCardPlayed(new CardPlayedEventArgs()
+                {
+                    Cards = cardsPlayed.ToArray(),
+                    GameNumber = GameNumber,
+                    StarterPlayer = playingOrder[0],
+                    TrickNumber = _currentTrickNumber,
+                    TrumpSuit = _trumpSuit,
+                    PlayerPlayingTheCard = position
+                });
+
+                if (inBetweenDelay.HasValue)
+                    await Task.Delay(inBetweenDelay.Value, cancellationToken);
+            }
+
+            int index = DecideWinnerCard(cardsPlayed, _trumpSuit);
+            _currentTrickStarter = playingOrder[index];
+            var outcome = new TrickOutcome()
+            {
+                Winner = _currentTrickStarter,
+                CardsPlayed = cardsPlayed,
+                TrumpUsage = GetUsage(cardsPlayed, _trumpSuit)
+            };
+
+            OnTrickFinished(new TrickFinishedEventArgs()
+            {
+                Outcome = outcome
+            });
+
+            foreach (var position in playingOrder)
+            {
+                var p = GetPlayer(position);
+                var banter = await p.InformTrickOutcomeAsync(outcome);
+                if (banter != null)
+                    OnBanterUttered(new BanterUtteredEventArgs() { Banter = banter, PlayerInfo = p });
+            }
+
+            return outcome;
+        }
+
+
+
+        internal static TrumpUsage GetUsage(List<Card> cards, Suit trumpSuit)
+        {
+            if (cards[0].Suit == trumpSuit)
+                return TrumpUsage.Mandatory;
+
+            return cards.Count(c => c.Suit == trumpSuit) switch
+            {
+                0 => TrumpUsage.NotUsed,
+                1 => TrumpUsage.UsedOnce,
+                _ => TrumpUsage.UsedMultiple
+            };
+        }
+
+        public static int DecideWinnerCard(List<Card> cards, Suit trumpSuit)
+        {
+            var copy = cards.ToList();
+            var startingSuit = copy[0].Suit;
+            var (cardValue, index) = copy.Select(
+                (card, index) => (GetCardValue(card, startingSuit, trumpSuit), index)).Max();
+            return index;
+        }
+
+        private static int GetCardValue(Card card, Suit startingSuit, Suit trumpSuit)
+        {
+            var value = ((int)card.Rank + 11) % 13;
+            if (card.Suit == trumpSuit)
+                value += 100;
+            if (card.Suit != startingSuit && card.Suit != trumpSuit)
+                return 0;
+            else return value;
+        }
+
+        public GameInfo ToGameInfo() => new GameInfo()
+        {
+            Score = Score,
+            GameNumber = GameNumber,
+            TrumpCaller = TrumpCaller,
+            CurrentTrick = CurrentTrick
+        };
     }
 }
