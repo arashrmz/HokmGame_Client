53467ac0fa1023aaf19b0cbdac82df04abd0449a
Arash <arash1.ramzani@gmail.com>
2021-12-24 21:56:41 +0330

    [Fix] Implemented methods

--- Assets/Scripts/Game/Players/RealPlayer.cs
+++ Assets/Scripts/Game/Players/RealPlayer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using HokmGame.Core.CardGame;
 using HokmGame.Core.Hokm;
@@ -10,55 +11,136 @@ using UnityEngine;
 
 namespace HokmGame.Game
 {
-    public class RealPlayer : MonoBehaviour, IPlayer
+    public class RealPlayer : IPlayer
     {
-        public Guid Id => throw new NotImplementedException();
+        private readonly CardManager cardManager;
+        private readonly PlayerManager playerManager;
+        public bool playNextMove;
+        private List<Card> currentPlayedDeck;
 
-        public string Name => throw new NotImplementedException();
+        public RealPlayer(string name, CardManager cardManager, PlayerManager playerManager)
+        {
+            Name = name;
+            Cards = new List<Card>();
+            this.cardManager = cardManager;
+            this.playerManager = playerManager;
+            playerManager.realPlayer = this;
+        }
+
+        public Guid Id => Guid.NewGuid();
+        public string Name { get; private set; }
+        public List<Card> Cards { get; private set; }
+        public TrickOutcome LastOutcome { get; private set; }
+        public PlayerPosition MyPosition { get; set; }
 
         public Task<string> BanterAsync()
         {
-            throw new NotImplementedException();
+            return Task.FromResult<string>("Wat??");
         }
 
         public Task<Suit> CallTrumpSuitAsync()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(Cards.First().Suit);
         }
 
         public Task<string> GameFinished(GameOutcome outcome, GameScore score)
         {
-            throw new NotImplementedException();
+            Debug.Log("Game finished");
+            return Task.FromResult<string>("Game Finished?");
         }
 
         public Task<string> InformTrickOutcomeAsync(TrickOutcome outcome)
         {
-            throw new NotImplementedException();
+            LastOutcome = outcome;
+            return Task.FromResult<string>("Wat??");
         }
 
         public Task MatchFinished(MatchScore score)
         {
-            throw new NotImplementedException();
+            Debug.Log("Match finished");
+            return Task.CompletedTask;
         }
 
         public Task NewGameAsync(MatchScore currentMatchScore, PlayerPosition caller)
         {
-            throw new NotImplementedException();
+            Cards = new List<Card>();
+            return Task.CompletedTask;
         }
 
         public Task NewMatchAsync(IDictionary<PlayerPosition, IPlayerInfo> playerInfos, PlayerPosition yourPosition)
         {
-            throw new NotImplementedException();
+            MyPosition = yourPosition;
+            return Task.CompletedTask;
         }
 
-        public Task<Card> PlayAsync(int trickNumber, IEnumerable<Card> playedByOthers, Suit trumpSuit)
+        public async Task<Card> PlayAsync(int trickNumber, IEnumerable<Card> playedByOthers, Suit trumpSuit)
         {
-            throw new NotImplementedException();
+            Debug.LogWarning("Your turn");
+            playerManager.ResetSelectedCard();
+            playNextMove = false;
+            playerManager.SelectCard(playedByOthers, trumpSuit);
+            while (!playNextMove)
+            {
+                await Task.Delay(1);
+            }
+            var selectedCard = playerManager.selectedCard;
+            return selectedCard;
+        }
+
+        internal bool CanPlayCard(Card card, IEnumerable<Card> playedByOthers, Suit trumpSuit)
+        {
+            var local = playedByOthers.ToArray();
+
+            //we are the first player of the round
+            if (local.Length == 0)
+                return true;
+
+            //playing a trump card
+            if (card.Suit == trumpSuit)
+                return true;
+
+            //player has atleast one card of the same suit, but has not chosen it
+            var sameSuit = Cards.FirstOrDefault(x => x.Suit == local[0].Suit);
+            if (sameSuit != null && card.Suit != sameSuit.Suit)
+                return false;
+
+            return true;
+        }
+
+        private Task<Card> FindACardAsync(int trickNumber, IEnumerable<Card> playedByOthers, Suit trumpSuit)
+        {
+            var local = playedByOthers.ToArray();
+
+            if (local.Length == 0)
+            {
+                //we are the first player of the round
+                return Task.FromResult(Cards.OrderBy(x => Guid.NewGuid()).First());
+            }
+
+            //get the first card with the same suit as round
+            var sameSuit = Cards.FirstOrDefault(x => x.Suit == local[0].Suit);
+            if (sameSuit != null)
+                return Task.FromResult<Card>(sameSuit);
+
+            //get the first card with the same rank as trump suit
+            var trump = Cards.FirstOrDefault(x => x.Suit == trumpSuit);
+            if (trump != null)
+                return Task.FromResult<Card>(trump);
+
+            //no card found, get the first card
+            return Task.FromResult(Cards.First());
         }
 
         public Task ReceiveHandAsync(IEnumerable<Card> cards)
         {
-            throw new NotImplementedException();
+            Cards.AddRange(cards);
+
+            if (Cards.Count != 5 && Cards.Count != 9 && Cards.Count != 13)
+                throw new InvalidOperationException($"The puppet has {Cards.Count}!");
+
+            if (Cards.Count == 13)
+                cardManager.SpawnDeck(Cards, MyPosition);
+            return Task.CompletedTask;
         }
     }
 }
