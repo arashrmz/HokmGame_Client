3e288e2ecd0009c489d959df1979be3158b4a76f
Arash Ramzani <arash1.ramzani@gmail.com>
2022-06-25 17:02:09 +0430

    GetTeammatePosition returns your teammate pos

--- Assets/Scripts/Core/Hokm/PlayerPosition.cs
+++ Assets/Scripts/Core/Hokm/PlayerPosition.cs
@@ -21,5 +21,18 @@ namespace HokmGame.Core.Hokm
         {
             return position == PlayerPosition.Team1Player1 || position == PlayerPosition.Team1Player2;
         }
+
+        //returns the position of teammate
+        public static PlayerPosition GetTeammatePosition(this PlayerPosition position)
+        {
+            if (position.IsTeam1())
+            {
+                return position == PlayerPosition.Team1Player1 ? PlayerPosition.Team1Player2 : PlayerPosition.Team1Player1;
+            }
+            else
+            {
+                return position == PlayerPosition.Team2Player1 ? PlayerPosition.Team2Player2 : PlayerPosition.Team2Player1;
+            }
+        }
     }
 }
